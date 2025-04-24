6604707546f35e7f5fa7f73164ae0e884b39f7d7
memesmith0 <meme.smith.wizard@gmail.com>
2025-04-24 02:10:20 -0700

    Update fastpyll.scm
    sodifijsodifosidfj

--- fastpyll.scm
+++ fastpyll.scm
@@ -680,3 +680,5 @@
 
 ;;langs[lang].append([[[j(thing.find_all('td')[1].find('h2').text.split()), j(thing.find_all('td')[1].find('p').text.split())]   for thing in (something.find_all('tr'))] for something in sp(file.read(), 'html.parser')] 
 (define key (lambda (x y) (concat x "[ " y " ]")))
+
+(define attribute (lambda (x y) (concat x "." y)))
