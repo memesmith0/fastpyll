ced4f2a281394efd977cc5cb690b43dc7534a4ec
memesmith0 <meme.smith.wizard@gmail.com>
2025-07-13 05:53:01 -0700

    Update fastpyll.sh
    sdfosdijfoidsjf

--- fastpyll.sh
+++ fastpyll.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-awk '{ if (NR == 1) { prev_line = "(fastpyll \x27(" $0; } else { print prev_line; prev_line = $0; } } END { if (NR > 0) { print prev_line "))"; } }' | guile fastpyll.scm > "$1" && python3 "$1"
+awk '{ if (NR == 1) { prev_line = "(fastpyll \x27(" $0; } else { print prev_line; prev_line = $0; } } END { if (NR > 0) { print prev_line "))"; } }' | guile fastpyll.scm > "$1"
