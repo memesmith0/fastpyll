c337fde33cbaa73e9f837a13c315d86245d60050
memesmith0 <meme.smith.wizard@gmail.com>
2025-07-13 03:51:04 -0700

    Update fastpyll.sh
    sdodfisodijfosdijf

--- fastpyll.sh
+++ fastpyll.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk '{ if (NR == 1) { prev_line = "(fastpyll \x27" $0; } else { print prev_line; prev_line = $0; } } END { if (NR > 0) { print prev_line ")"; } }'
+awk '{ if (NR == 1) { prev_line = "(fastpyll \x27" $0; } else { print prev_line; prev_line = $0; } } END { if (NR > 0) { print prev_line ")"; } }' | guile fastpyll.scm
