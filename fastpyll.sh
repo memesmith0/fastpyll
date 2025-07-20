a15a810e19b668df64d2492aad1f92aa9cfddf6e
memesmith0 <meme.smith.wizard@gmail.com>
2025-07-20 07:16:27 -0700

    Update fastpyll.sh
    sodifosdifjosdifj

--- fastpyll.sh
+++ fastpyll.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk '{ if (NR == 1) { prev_line = "(fastpyll \x27(\n\n" $0; } else { print prev_line; prev_line = $0; } } END { if (NR > 0) { print prev_line "\n))"; } }' | guile fastpyll.scm 
+awk '{ if (NR == 1) { prev_line = "(fastpyll_fastpyll \x27(\n\n" $0; } else { print prev_line; prev_line = $0; } } END { if (NR > 0) { print prev_line "\n))"; } }' | guile fastpyll.scm 
